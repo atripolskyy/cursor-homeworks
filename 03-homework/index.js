c6b031154c443bd8502e4df663b68408e9eaa0f7
Oleksii Trypolskyi <atripolskyy@gmail.com>
2020-07-23 02:03:15 +0300

    03-homework: add tasks number 7 and 9

--- 03-homework/index.js
+++ 03-homework/index.js
@@ -53,9 +53,45 @@ const countLetter = (character, word) => {
   return count;
 }
 
+/**
+ * Task #7. Створіть функцію, яка конвертує долари в гривні та навпаки в залежності від наявності символа $ або UAH в рядку.
+ * Врахуйте, інші валюти не конвертуються, потрібно виводити помилку, і також регістр uah не має значення.
+ */
+const convertCurrency = value => {
+  const CURRENCY_RATE = 25; // $1 = 25UAH
+  const regex = /(\d*)(\$|UAH|uah)/g;
+  const found = [...value.matchAll(regex)];
+  if (found.length === 0) return `Помилка! Введіть долари або гривні. Приклад: 1$, 1uah, 1UAH.`;
+
+  const amount = found[0][1];
+  const currency = found[0][2].toLowerCase();
+
+  switch (currency) {
+    case '$':
+      return `${amount * CURRENCY_RATE} грн.`;
+    case 'uah':
+      return `${amount / CURRENCY_RATE}$`;
+    default:
+      return `Помилка! Введіть долари або гривні. Приклад: 1$, 1uah, 1UAH.`;
+  }
+}
+
+/**
+ * Task #9. Створіть функцію, яка видаляє всі букви з речення.
+ */
+const deleteLetters = (character, word) => {
+  word = word.toLowerCase();
+  character = character.toLowerCase();
+  const regExpPattern = new RegExp(character, `g`);
+
+  return word.replace(regExpPattern, ``);
+}
+
 console.log(`Task #1: `, getMaxDigit(1236));
 console.log(`Task #2: `, getPow(2, 4));
 console.log(`Task #3: `, getFirstUpper(`вЛАД`));
 console.log(`Task #4: `, getSalary(1000));
 console.log(`Task #5: `, getRandomNumber(1, 10));
 console.log(`Task #6: `, countLetter(`а`, `Асталавіста`));
+console.log(`Task #7: `, convertCurrency(`250`));
+console.log(`Task #9: `, deleteLetters(`a`, `blablabla`));
