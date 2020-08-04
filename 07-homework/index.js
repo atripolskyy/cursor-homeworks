3bad651fb884db5c7f8fd1c8e846705daf1bb8e6
Oleksii Trypolskyi <atripolskyy@gmail.com>
2020-08-04 03:10:33 +0300

    07-homework

---
+++ 07-homework/index.js
@@ -0,0 +1,11 @@
+const ukraine = { tax: 0.195, middleSalary: 1789, vacancies: 11476 };
+const latvia = { tax: 0.25, middleSalary: 1586, vacancies: 3921 };
+const litva = { tax: 0.15, middleSalary: 1509, vacancies: 1114 };
+
+const getMyTaxes = () => {
+  return 1;
+}
+
+const getMiddleTaxes = () => {
+
+}
